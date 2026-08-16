700b57ddf1e8db5d87099fecfc27191abc6837c9
TheAio <76646135+TheAio@users.noreply.github.com>
2026-08-16 17:41:04 +0200

    Add update.lua

---
+++ main/update.lua
@@ -0,0 +1,30 @@
+shell.run("wget https://raw.githubusercontent.com/TheAio/cc-aios-x26/refs/heads/main/ver /.sys/tmp/short/.ver")
+if fs.exists("/.sys/tmp/short/.ver") and fs.exists("/.sys/common/.ver") then
+    j = fs.open("/.sys/common/.ver","r")
+    h = fs.open("/.sys/tmp/short/.ver","r")
+    if h.readLine() ~= j.readLine() then
+        term.setBackgroundColor(colors.black)
+        term.setCursorPos(1,1)
+        term.clear()
+        printError("New major update avaible, manual intervention needed!")
+        sleep(5)
+    end
+    if h.readLine() ~= j.readLine() then
+        term.setBackgroundColor(colors.black)
+        term.setCursorPos(1,1)
+        term.clear()
+        printError("System update in progress, please wait...")
+        while true do
+            k=h.readLine()
+            if k == nil then break end
+            shell.run(k)
+            sleep(0)
+        end
+        sleep(3)
+        h.close()
+        j.close()
+        os.reboot()
+    end
+    h.close()
+    j.close()
+end
